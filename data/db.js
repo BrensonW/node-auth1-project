fe64e883ad261556068e2ad63ade49c1a5545ad0
Brenson <brensonw300@gmail.com>
2020-11-09 11:24:43 -0800

    Tested and server works and so project is complete

--- data/db.js
+++ data/db.js
@@ -1,5 +1,5 @@
 const knex = require('knex')
-const enviroment = process.env.NODE_ENV || 'development'
+const environment = process.env.NODE_ENV || 'development'
 const config = require('../knexfile')[environment]
 
 module.exports = knex(config);
